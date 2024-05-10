b1d7e769beeb5dfaf3447745743deb13bfce18f1
Ranjan Kumar <134587670+Ranjan221120@users.noreply.github.com>
2024-05-11 00:44:17 +0530

    Bit Manupluation

---
+++ CountOne.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class CountOne{
+    public static void main(String ar[]){
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Number");
+        int n =sc.nextInt();
+        int nn =n;
+        System.out.println("");
+        
+        //M-1(Brian Kernighon's Algorithm) 
+        System.out.println("Method- 1");
+        int c=0;
+        while(n>0){
+            n=n &(n-1);
+            c++;
+        }
+        System.out.println("Count the no. of 1 in "+nn+" : "+c);
+        System.out.println("");
+
+        System.out.println("Method- 2");
+        System.out.println("Count the no. of 1 in "+nn+" : "+Integer.bitCount(nn));
+
+
+
+        System.out.println("");
+        var x= Integer.toBinaryString(nn);
+        System.out.print("Convert "+nn+" Decimal to Binary: ");
+        System.out.println(x);
+        System.out.print("Convert "+x +" Binary to Decimal: ");
+        System.out.println(Integer.parseInt(String.valueOf(x),2));
+        
+
+    }
+}
---
+++ ToggleBit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ToggleBit{
+    public static void main(String ar[]){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the no.");
+            int n = sc.nextInt();
+            System.out.println("Enter the Position");
+            int pos = sc.nextInt();
+
+            int mask =1<< pos;
+
+            System.out.println(Integer.toBinaryString(n));
+            System.out.println(n^mask);
+            System.out.println(Integer.toBinaryString(n^mask));
+        }
+    }
+}
