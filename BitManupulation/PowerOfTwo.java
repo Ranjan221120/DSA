3e9c3293295399e90c2dfba7f1d05b44a9342301
Ranjan Kumar <kranjan7417@gmail.com>
2024-05-11 01:04:17 +0530

    Changes path location of bit manupulation

---
+++ BitManupulation/PowerOfTwo.java
@@ -0,0 +1,27 @@
+package BitManupulation;
+import java.util.*;
+
+class PowerOfTwo{
+    public static void main(String ar[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int n =sc.nextInt();
+		if(isPowerTwo(n)){
+		 System.out.println(n+" is Power of 2");
+		}else{
+		 System.out.println(n+" is not Power of 2");
+		}
+	}
+
+	/* static boolean isPowerTwo(int n){
+			while(n%2==0){
+				n/=2;
+				if(n==1)return true;
+			  }
+		return false;
+	} */
+
+	static boolean isPowerTwo(int n){		
+	return (n > 0) && ((n & (n - 1)) == 0);
+}
+} 
